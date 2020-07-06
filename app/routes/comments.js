18ba83ec565bb1c46ce136654b78642e58f33fe5
Tillaine <55994239+Tillaine@users.noreply.github.com>
2020-07-06 16:59:44 -0600

    finished auth and restful routes for comments

--- app/routes/comments.js
+++ app/routes/comments.js
@@ -3,12 +3,35 @@ const   express = require("express"),
 const   Spot   = require("../models/Spot"),
         Comment = require("../models/comment");
 
+// ************************
 //middleware 
+// ************************
 const   isLoggedIn = (req, res, next) => {
     if( req.isAuthenticated() ) {
         return next();
     }
     res.redirect("/login");
+}
+
+const checkCommentOwnership = (req, res, next) => {
+    if(req.isAuthenticated()) {
+        console.log('id', req.params.comment_id)
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            console.log('comment', foundComment)
+            if ( err ) { 
+                console.log(err) 
+                res.redirect('back')}
+            else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                   next();
+                } else {
+                    res.redirect('back')
+                }
+            }
+        });
+    } else {
+        res.redirect('back')
+    }
 }
 
 //comments new
@@ -44,7 +67,44 @@ router.post("/", isLoggedIn, (req, res) => {
 
 //edit comment 
 
-router.get('/comment_id/edit')
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
+    const   spotID = req.params.id,
+            commentID = req.params.comment_id;
+    Comment.findById(commentID, (err, comment) => {
+        if ( err ) {
+            console.log(err)
+            res.redirect('back')
+        } else {
+            res.render('../views/comments/edit', { spotID, comment });
+             
+        }
+    })
+    
+})
 
+//update comment 
+
+router.put('/:comment_id', (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, checkCommentOwnership, req.body.comment, (err, comment) => {
+        if ( err ) {
+            console.log(err);
+            res.redirect('back');
+        } else {
+            res.redirect("/spots/" + req.params.id);
+        }
+    })
+})
+
+// detele comment 
+
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
+    console.log('comment id', req.params.comment_id)
+    Comment.findByIdAndDelete(req.params.comment_id, (err) => {
+        if ( err ) {
+            console.log(err)
+            res.redirect("/spots/" + req.params.id);
+        }
+    })
+});
 
 module.exports = router;
